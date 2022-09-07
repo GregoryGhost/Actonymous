d52fbe228681757588dfb2e12d77c0a3fb1e1a09
Gregory <lobster287@gmail.com>
2022-09-08 01:25:03 +0700

    Formatted py code with black

--- APIs/JiraWorklogManager/jira_dtos.py
+++ APIs/JiraWorklogManager/jira_dtos.py
@@ -5,7 +5,7 @@ from typing import List, Tuple, final
 
 @final
 class UserWorklogInfo:
-    comment: str = ''
+    comment: str = ""
     created_date: datetime = None  # type: ignore
     time_spent_seconds: int = 0
 
@@ -26,8 +26,8 @@ JiraWorklogs = List[UserWorklogInfo]
 
 @final
 class JiraIssue:
-    code: str = ''
-    name: str = ''
+    code: str = ""
+    name: str = ""
     worklogs: JiraWorklogs = []
 
     def __init__(self, code: str, name: str, worklogs: JiraWorklogs):
@@ -47,11 +47,13 @@ JiraBasicAuth = Tuple[str, str]
 
 @final
 class JiraInfo:
-    jira_server_address: str = ''
-    basic_auth: JiraBasicAuth = ('login', 'password')
-    user_login: str = ''
+    jira_server_address: str = ""
+    basic_auth: JiraBasicAuth = ("login", "password")
+    user_login: str = ""
 
-    def __init__(self, user_login: str, jira_server_address: str, basic_auth: JiraBasicAuth):
+    def __init__(
+        self, user_login: str, jira_server_address: str, basic_auth: JiraBasicAuth
+    ):
         self.user_login = user_login
         self.jira_server_address = jira_server_address
         self.basic_auth = basic_auth
@@ -59,20 +61,28 @@ class JiraInfo:
 
 @final
 class UserWorklogItemDto:
-    task_code: str = ''
-    task_name: str = ''
-    start_period_date: datetime = None   # type: ignore
+    task_code: str = ""
+    task_name: str = ""
+    start_period_date: datetime = None  # type: ignore
     end_period_date: datetime = None  # type: ignore
     time_spent_seconds: int = 0
 
-    def __init__(self, task_code: str, task_name: str, start_period_date: datetime, end_period_date: datetime, time_spent_seconds: int):
+    def __init__(
+        self,
+        task_code: str,
+        task_name: str,
+        start_period_date: datetime,
+        end_period_date: datetime,
+        time_spent_seconds: int,
+    ):
         self.task_code = task_code
         self.task_name = task_name
         self.start_period_date = start_period_date
         self.end_period_date = end_period_date
         self.time_spent_seconds = time_spent_seconds
 
+
 @final
 class UserWorklogItemDtoEncoder(json.JSONEncoder):
     def default(self, o: UserWorklogItemDto):
-        return o.__dict__ 
+        return o.__dict__
--- APIs/JiraWorklogManager/jira_helpers.py
+++ APIs/JiraWorklogManager/jira_helpers.py
@@ -8,11 +8,14 @@ from jira.resources import Issue
 def get_user_worklogs(issue: Issue, user_login: str) -> jira_dtos.JiraWorklogs:
     worklogs = issue.fields.worklog.worklogs
     worklogs = filter(lambda x: x.author.name == user_login, worklogs)
-    worklogs = map(lambda x: jira_dtos.UserWorklogInfo(
-        comment = x.comment,
-        created_date = x.created,
-        time_spent_seconds = x.timeSpentSeconds
-    ), worklogs)
+    worklogs = map(
+        lambda x: jira_dtos.UserWorklogInfo(
+            comment=x.comment,
+            created_date=x.created,
+            time_spent_seconds=x.timeSpentSeconds,
+        ),
+        worklogs,
+    )
     worklogs = list(worklogs)
 
     return worklogs
@@ -23,11 +26,7 @@ def map_issues(issue: Issue, user_login: str) -> jira_dtos.JiraIssue:
     issue_name = issue.fields.summary
     worklogs = get_user_worklogs(issue, user_login)
     # json_worklogs = json.dumps(worklogs, cls = UserWorklogInfoEncoder)
-    mapped = jira_dtos.JiraIssue(
-        code = issue_code,
-        name = issue_name,
-        worklogs = worklogs
-    )
+    mapped = jira_dtos.JiraIssue(code=issue_code, name=issue_name, worklogs=worklogs)
 
     return mapped
 
@@ -38,7 +37,7 @@ WorklogPeriod = Optional[Tuple[datetime, datetime]]
 def get_worklogs_period(worklogs: jira_dtos.JiraWorklogs) -> WorklogPeriod:
     if not worklogs:
         return None
-    
+
     select_dates = lambda x: x.created_date
     start_period = min(worklogs, key=select_dates).created_date
     end_period = max(worklogs, key=select_dates).created_date
@@ -65,11 +64,11 @@ def map_user_worklog(user_worklog: jira_dtos.JiraIssue) -> MappedUserWorklog:
     start_period_date, end_period_date = period
     time_spent_seconds = get_time_spent(user_worklog.worklogs)
     user_worklog_item = jira_dtos.UserWorklogItemDto(
-        task_code = user_worklog.code,
-        task_name = user_worklog.name,
-        start_period_date = start_period_date,
-        end_period_date = end_period_date,
-        time_spent_seconds = time_spent_seconds
+        task_code=user_worklog.code,
+        task_name=user_worklog.name,
+        start_period_date=start_period_date,
+        end_period_date=end_period_date,
+        time_spent_seconds=time_spent_seconds,
     )
 
     return user_worklog_item
