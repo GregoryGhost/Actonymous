d70b63f090d3731e9c018cbef8ced1d8e3d9277e
Gregory <lobster287@gmail.com>
2022-12-17 10:51:11 +0700

    Added dockerfile for DocsReporter

--- APIs/DocsReporter/docs_reporter/__main__.py
+++ APIs/DocsReporter/docs_reporter/__main__.py
@@ -9,7 +9,7 @@ import generated.grpc.protos.API.docs_reporter_pb2_grpc as docs_reporter_pb2_grp
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     docs_reporter_pb2_grpc.add_JiraWorklogManagerServicer_to_server(docs_reporter_api_service.DocsReporterApiService(), server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port("[::]:80")
     server.start()
     server.wait_for_termination()
 
