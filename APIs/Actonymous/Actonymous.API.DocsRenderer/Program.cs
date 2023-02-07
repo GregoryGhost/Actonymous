b65f5a5f82669ea1de3842a5162416c5ff4b4e94
Gregory <lobster287@gmail.com>
2023-02-07 23:43:20 +0700

    Added draft tex2pdf renderer implementation.

---
+++ APIs/Actonymous/Actonymous.API.DocsRenderer/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+app.Run();
---
+++ APIs/Actonymous/Actonymous.API.DocsRenderer/Services/Tex2PdfRenderService.cs
@@ -0,0 +1,32 @@
+﻿namespace Actonymous.API.DocsRenderer.Services;
+
+using Grpc.Core;
+
+using JetBrains.Annotations;
+
+using Tex2PdfRenderer.V1;
+
+[PublicAPI]
+public sealed class RenderService : Tex2PdfRenderer.Tex2PdfRendererBase
+{
+    private readonly PdfPackageDto _emptyPdfPackage;
+
+    public RenderService()
+    {
+        _emptyPdfPackage = new PdfPackageDto();
+    }
+    
+    /// <inheritdoc />
+    public override async Task Render(IAsyncStreamReader<TexPackageDto> requestStream, IServerStreamWriter<PdfPackageDto> responseStream, ServerCallContext context)
+    {
+        await foreach (var _ in requestStream.ReadAllAsync(context.CancellationToken))
+        {
+            await NotifyGettingSettingsEndAsync(responseStream, _emptyPdfPackage);
+        }
+    }
+
+    private static async Task NotifyGettingSettingsEndAsync(IServerStreamWriter<PdfPackageDto> responseStream, PdfPackageDto pdfPackage)
+    {
+        await responseStream.WriteAsync(pdfPackage);
+    }
+}
